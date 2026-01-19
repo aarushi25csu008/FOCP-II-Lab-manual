6d46113c2d505f255b6f9ec0fa07f191a24a4eaa
aarushi25csu008 <aarushi25csu008@ncuindia.edu>
2026-01-19 11:09:19 +0530

    Add files via upload
    lab manual question

---
+++ Q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // inputing 2 numbers and adding them
+    int num1,num2;
+    cout<<"enter the first number: ";
+    cin>>num1;
+    cout<<"enter the second number: ";
+    cin>>num2;
+    cout<<"the sum of the numbers is:"<<num1+num2;
+    return 0;
+}
