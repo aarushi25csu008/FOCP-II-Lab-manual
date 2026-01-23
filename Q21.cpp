9907530ba110637bce41d091abc9ec5353846275
aarushi25csu008 <aarushi25csu008@ncuindia.edu>
2026-01-23 19:48:40 +0530

    Update Q21.cpp

--- Q21.cpp
+++ Q21.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
+//programme for printing a hollow 
 int main(){
-    int i,n;
-    for(i=0;i<=5;i++){
-        for(n=0;n<=5;n++){
-            if(i==1||i==5||n==0||n==6){
-            cout<<"*";}
-            else{
-                cout<<" ";
-            }
+  int i,j;
+  for(i=0;i<6;i++){
+    for(j=0;j<6;j++){
+        if(i==0||i==5||j==0||j==5){
+        cout<<"*";}
+        else{
+            cout<<" ";
         }
-        cout<<endl;
     }
+    cout<<endl;
+  }
 }
